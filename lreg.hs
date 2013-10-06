db6c321d6db2f64bd26a6e491418bc282486827b
Ivan Acosta-Rubio <ivan@bakedweb.net>
2013-10-06 16:27:30 -0500

    inlining those functions

--- lreg.hs
+++ lreg.hs
@@ -1,18 +1,13 @@
 module Prediction where
 
-multpr :: (Floating a) => (a,a) -> a 
-multpr (x,y) = x * y
-
-exppr :: (Floating a) => (a,a) -> a
-exppr (x,y) = x * x
-
-lReg :: (Floating a) => a -> [(a,a)] -> a
+lReg :: (Fractional a) => a -> [(a,a)] -> a
 lReg x xs = let n = fromIntegral( length xs )
                 sumx = sum $ map fst xs
                 sumy = sum $ map snd xs
                 sumxy = sum $ map multpr xs
                 sumxx = sum $ map exppr xs
+                multpr (x,y) = x * y
+                exppr (x,y) = x * x
                 slope = ( n * sumxy - sumx *  sumy)  / ( n * sumxx - sumx ^ 2 )
                 intercept = (sumy - slope * sumx) / n
                 in intercept + slope * x
-
