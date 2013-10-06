64991e718b2a811ccb8a301006a1075f93897f63
Ivan Acosta-Rubio <ivan@bakedweb.net>
2013-10-06 15:11:44 -0500

    refactoring

--- lreg.hs
+++ lreg.hs
@@ -8,11 +8,11 @@ exppr (x,y) = x * x
 
 lReg :: (Floating a) => a -> [(a,a)] -> a
 lReg x xs = let n = fromIntegral( length xs )
-                sumx = sum (map fst xs)
-                sumy = sum (map snd xs)
-                sumxy = sum (map multpr xs)
-                sumxx = sum ( map exppr xs)
-                slope = ( (n * sumxy) - (sumx *  sumy) )  / ( (n * sumxx) - (sumx ^ 2) )
-                intercept = (sumy - (slope * sumx)) / n
+                sumx = sum $ map fst xs
+                sumy = sum $ map snd xs
+                sumxy = sum $ map multpr xs
+                sumxx = sum $ map exppr xs
+                slope = ( n * sumxy - sumx *  sumy)  / ( n * sumxx - sumx ^ 2 )
+                intercept = (sumy - slope * sumx) / n
                 in intercept + slope * x
 
